528290025df41b9bf5b918ed9dd8a04cd5700f85
Ana <anastasiiashershneva@gmail.com>
2022-09-22 23:40:02 +0300

    changed file Users

--- tests/test_data/users.py
+++ tests/test_data/users.py
@@ -1,12 +1,16 @@
-ana = {
-    'name': 'Ana',
-    'surname': 'Sher',
-    'email': 'email@mail.com',
-    'phone': '9991234567',
-    'b_day': '31 October 1995',
-    'gender': 'Female',
-    'subjects': 'sport',
-    'hobbies': 'reading',
-    'prof_picture': 'pic.jpg',
-    'state_and_city': 'NCR Delhi'
-}
+from dataclasses import dataclass
+@dataclass()
+class User:
+    name: str
+    surname: str = 'Sher'
+    email: str = 'email@mail.com'
+    phone: str = '9991234567'
+    b_day: str = '31 October 1995'
+    gender: str = 'Female'
+    subjects: str ='sport'
+    hobbies: str ='reading'
+    prof_picture: str = 'pic.jpg'
+    state_and_city: str = 'NCR Delhi'
+
+
+ana = User(name='Ana')
